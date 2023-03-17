7999205f22ff4ac850f535c2be5ca05045659c87
Grony <gr0ny.0017@gmail.com>
2023-03-17 18:53:29 +0300

    Нулевка

--- webapp/pages/_app.tsx
+++ webapp/pages/_app.tsx
@@ -9,7 +9,12 @@ function MyApp({Component, pageProps}: AppProps): JSX.Element {
             <Head>
                 <title>TerraQuiz</title>
                 <link rel="icon" href="/favicon.ico"/>
-                <link rel="preconnect" href="https://fonts.gstatic.com"/>
+
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+                <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+                      rel="stylesheet"/>
+
             </Head>
             <Component {...pageProps} />
         </>
