26a2e36af93bda7b50eb2e59eec3ebda329fc959
Grony <gr0ny.0017@gmail.com>
2023-03-19 02:25:58 +0300

    footer

--- webapp/layout/Footer/Footer.tsx
+++ webapp/layout/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import { FooterProps } from './Footer.props';
 import cn from 'classnames';
+import styles from './Footer.module.css';
+import { format } from 'date-fns';
 
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<footer className={cn(className)} {...props}>
-			Footer
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div className={styles.footerWrapper}>
+        <div className={styles.copyright}>
+          Terra Quiz © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+        </div>
+      </div>
 		</footer>
 	);
 };
