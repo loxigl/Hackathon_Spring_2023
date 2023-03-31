1751ba461d509db87ed2aba358dec1c2df243ae8
Grony <gr0ny.0017@gmail.com>
2023-04-01 02:23:34 +0300

    Map

--- webapp/page-components/MainPage/MainPage.tsx
+++ webapp/page-components/MainPage/MainPage.tsx
@@ -1,7 +1,7 @@
 import styles from './MainPage.module.css';
 import {MainPageProps} from './MainPage.props';
 import cn from 'classnames';
-import {Hello, MapComponent} from '@/components';
+import {Hello} from '@/components';
 
 
 export const MainPage = ({className, ...props}: MainPageProps): JSX.Element => {
@@ -9,7 +9,6 @@ export const MainPage = ({className, ...props}: MainPageProps): JSX.Element => {
   return (
     <div className={cn(className, styles.mainPage)} {...props}>
       <Hello/>
-
     </div>
   );
 };
