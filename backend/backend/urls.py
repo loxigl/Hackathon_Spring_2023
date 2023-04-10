7b7fcd2891b8f96bca443d0759711b5ad5d6b7f2
loxigl <43906792+loxigl@users.noreply.github.com>
2023-04-11 00:17:47 +0300

    media

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from django.views.decorators.csrf import csrf_exempt
+
+from .getmedia import get_media_path
 
 urlpatterns = [
     path('api/admin/', admin.site.urls),
     path('api/quiz/', include('quiz.urls')),
+    path('api/media/<path>', get_media_path, name='media')
 ]
